848f1b03a8798f2ae03f2e5bccccd18c0c503cc0
Alex Ahumada <projectsperminute@gmail.com>
2022-06-27 22:40:33 +0200

    Add density

--- demo1/src/App.tsx
+++ demo1/src/App.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react'
 import * as L from 'leaflet'
-import { Layer } from 'leaflet'
+import {geoJSON, Layer} from 'leaflet'
 import { Feature } from 'geojson';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import './App.css'
@@ -12,14 +12,19 @@ function App() {
         reference: null,
     })
 
+    const [maxDensity, setMaxDensity] = useState(0);
+
     useEffect( () => {
-        fetch('/data/cartografia_EPSG4326_censo2022_cadiz.json')
+        fetch('/data/distritos-censales-densidad-poblacion-2021-cartografia-EPSG4326-v1.0.0.json')
           .then(response => response.json())
           .then(data => { setMap({
-              geoJSON: data,
-              reference: null,
+                geoJSON: data,
+                reference: null,
           })})
     }, [])
+
+    //setMaxDensity(map.geoJSON.features.reduce((p, c) => p.properties.DEN > c.properties.DEN ? p : c))
+    //console.log(map.geoJSON.features.reduce((p, c) => p.properties.DEN > c.properties.DEN ? p : c))
 
     const position = {
         lat: 36.51672,
@@ -30,15 +35,16 @@ function App() {
       height: '100vh'
     }
 
-    const featureStyle = {
+    const featureStyle = (feature: Feature) => ({
         color: '#ff0000',
         weight: 1.5,
-        opacity: 0.65
-    }
+        opacity: 0.9,
+        fillOpacity: feature.properties?.DEN.replace(/,/g, '.') / 146100.0
+    })
 
     const onEachFeature = (feature:Feature, layer:Layer) => {
-        if (feature.properties?.CUSEC) {
-          layer.bindTooltip(feature.properties.CUSEC)
+        if (feature.properties?.DEN) {
+            layer.bindTooltip(`Extensión: ${feature.properties.EXT.replace(/,/g, '.')} | Población: ${feature.properties.POB.replace(/,/g, '.')} | Densidad de población: ${feature.properties.DEN.replace(/,/g, '.')}`);
         }
     }
 
